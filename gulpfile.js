de444c9949712388a7495236ee66a41aabe5da83
hbl917070 <hbl917070@hotmail.com.tw>
2026-04-24 00:58:56 +0800

    Correct async handling in gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 import fs from 'fs';
+import path from 'path';
 import gulp from 'gulp';
 import ejs from 'gulp-ejs';
 import rename from 'gulp-rename';
 import gulpEsbuild from 'gulp-esbuild';
 import sassFactory from 'gulp-sass';
-import sassCompiler from 'sass';
+import * as sassCompiler from 'sass';
 import { I18n } from "./I18n.js";
 import { langData } from "./I18n.js";
 
@@ -13,11 +14,11 @@ var i18n = new I18n();
 i18n.setData(langData);
 
 // scss 轉 css
-gulp.task("scss", async () => {
-    gulp.src("./scss/**/*.scss") // 指定要處理的 Scss 檔案目錄
+gulp.task("scss", () => {
+    return gulp.src("./scss/**/*.scss") // 指定要處理的 Scss 檔案目錄
         .pipe(sass({
             //outputStyle: "compressed", // 壓縮
-        }))
+        }).on("error", sass.logError))
         .pipe(gulp.dest("./css")) // 指定編譯後的 css 檔案目錄
 });
 
@@ -34,6 +35,8 @@ gulp.task("ejs", async () => {
         "./ejs/support.ejs", // 常見問題
     ];
 
+    const streams = [];
+
     for (let i = 0; i < arLang.length; i++) {
         const lang = arLang[i].lang;
         const dest = arLang[i].dest;
@@ -41,28 +44,40 @@ gulp.task("ejs", async () => {
             const src = arSrc[j];
             const urlRoot = '.'.repeat(dest.match(/[/]/g).length);
 
-            gulp.src(src)
-                .pipe(ejs({
-                    readFile: readFile,
-                    lang: lang,
-                    root: urlRoot,
-                    t: (key, value) => { return i18n.t(key, value, lang) },
-                }, { async: true }))
-                .pipe(rename({ extname: ".html" })) // 修改輸出的副檔名
-                .pipe(gulp.dest(dest))
+            streams.push(new Promise((resolve, reject) => {
+                gulp.src(src)
+                    .pipe(ejs({
+                        readFile: readFile,
+                        lang: lang,
+                        root: urlRoot,
+                        t: (key, value) => { return i18n.t(key, value, lang) },
+                    }, { async: true }))
+                    .on("error", reject)
+                    .pipe(rename({ extname: ".html" })) // 修改輸出的副檔名
+                    .pipe(gulp.dest(dest))
+                    .on("end", resolve)
+                    .on("error", reject);
+            }));
         }
     }
+
+    return Promise.all(streams);
 });
 
 // ts 轉 js
 gulp.task("ts", async () => {
-    gulp.src("./ts/app.ts")
-        .pipe(gulpEsbuild({
-            // minify: true, // 壓縮
-            outfile: "app.js",
-            bundle: true,
-        }))
-        .pipe(gulp.dest("./js"))
+    return new Promise((resolve, reject) => {
+        gulp.src("./ts/app.ts")
+            .pipe(gulpEsbuild({
+                // minify: true, // 壓縮
+                outfile: path.basename("./ts/app.ts", ".ts") + ".js",
+                bundle: true,
+            }))
+            .on("error", reject)
+            .pipe(gulp.dest("./js"))
+            .on("end", resolve)
+            .on("error", reject);
+    });
 });
 
 // 打包 - 持續監控檔案變化
@@ -90,7 +105,11 @@ gulp.task("build", gulp.series(
 async function readFile(path) {
     let t = await new Promise((resolve, reject) => {
         fs.readFile(path, "utf8", function (err, data) {
-            resolve(data); //繼續往下執行
+            if (err) {
+                resolve("");
+                return;
+            }
+            resolve(data); // 繼續往下執行
         });
     })
     return t;
